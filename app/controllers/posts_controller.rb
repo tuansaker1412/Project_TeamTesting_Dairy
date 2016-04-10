13cf8abca910b5b8b1b4b307d46d8144c969928b
Pham Huy Manh <huymanhtmhp@gmail.com>
2016-04-10 20:33:54 +0000

    xong phan quan ly nguoi dung

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   end
   
   def new
-    @post = Post.new
+    @post = current_user.posts.build
   end
   
   def show
@@ -25,7 +25,7 @@ class PostsController < ApplicationController
   end
   
   def create
-    @post = Post.new(post_params)
+    @post = current_user.posts.build(post_params)
     if @post.save
       redirect_to @post
     else
